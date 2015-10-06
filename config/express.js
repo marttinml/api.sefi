405345912bfec0cfde9761b5416f9c4464893f76
marttinml <martin.ml@icloud.com>
2015-10-06 00:41:11 -0500

    all
    *

--- config/express.js
+++ config/express.js
@@ -13,7 +13,7 @@ module.exports = function(){
     app.use(function (req, res, next) {
 
     // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'http://192.168.0.17');
+    res.setHeader('Access-Control-Allow-Origin', '*');
 
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
