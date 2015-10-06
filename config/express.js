1d089eaddb356200be7ccdca9fb4062297389b48
marttinml <martin.ml@icloud.com>
2015-10-05 21:09:25 -0500

    change
    add origin access

--- config/express.js
+++ config/express.js
@@ -13,7 +13,7 @@ module.exports = function(){
     app.use(function (req, res, next) {
 
     // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost');
+    res.setHeader('Access-Control-Allow-Origin', 'http://192.168.0.17');
 
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
