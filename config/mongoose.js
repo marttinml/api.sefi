285a68bff87d037af6094291b745ba5e89ccb550
marttinml <martin.ml@icloud.com>
2015-10-07 01:34:03 -0500

    FSD
    SDFDSA

--- config/mongoose.js
+++ config/mongoose.js
@@ -6,7 +6,10 @@ var MongoClient = require('mongodb').MongoClient
 module.exports = function(){
     //var db = mongoose.connect('mongodb://localhost/sefi', function (err, res) {
     //var db = mongoose.connect('mongodb://usrsefi:passsefi@ds029814.mongolab.com:29814/sefi', function (err, res) {
-    var url = 'mongodb://usrsefi:passsefi@ds029814.mongolab.com:29814/sefi';
+    
+    //var url = 'mongodb://usrsefi:passsefi@ds029814.mongolab.com:29814/sefi';
+    var url = 'mongodb://localhost/sefi';
+
     //var db = mongoose.connect('mongodb://ccles:Ccles13@ds051980.mongolab.com:51980/ccles', function(err, res) {
     var db = MongoClient.connect(url, function(err, db) { 
         if (err) {
